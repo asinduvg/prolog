4a883b2dcbc759a2e1002d291bd41cae78a9aa02
Asindu Vidanagamachchi <ashvg.dev@gmail.com>
2024-12-21 13:29:05 +0530

    Map coloring with Brazil

--- programmer-passport-prolog/colors/db.pl
+++ programmer-passport-prolog/colors/db.pl
@@ -1,17 +1,22 @@
-different(yellow, red).
-different(red, yellow).
-different(yellow, blue).
-different(blue, yellow).
-different(blue, red).
-different(red, blue).
+border(Country1, Country2) :-
+    color(Country1),
+    color(Country2),
+    dif(Country1, Country2).
 
-coloring(Chilie, Argentina, Bolivia, Uruguay, Paraguay, Peru) :-
-    different(Chilie, Argentina),
-    different(Chilie, Bolivia),
-    different(Chilie, Peru),
-    different(Argentina, Bolivia),
-    different(Argentina, Paraguay),
-    different(Uruguay, Argentina),
-    different(Uruguay, Paraguay),
-    different(Bolivia, Paraguay),
-    different(Bolivia, Peru).
+color(Color) :-
+    member(Color, [red, yellow, blue, green]).
+
+coloring(Chilie, Argentina, Bolivia, Uruguay, Paraguay, Peru, Brazil) :-
+    border(Chilie, Argentina),
+    border(Chilie, Bolivia),
+    border(Chilie, Peru),
+    border(Argentina, Bolivia),
+    border(Argentina, Paraguay),
+    border(Uruguay, Argentina),
+    border(Uruguay, Paraguay),
+    border(Bolivia, Paraguay),
+    border(Bolivia, Peru),
+    border(Brazil, Peru),
+    border(Brazil, Paraguay),
+    border(Brazil, Bolivia),
+    border(Brazil, Uruguay).
